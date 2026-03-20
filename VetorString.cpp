33db98bea8a4f5ecd273b08d02d8d3fdb5cc374e
home <abiliomatute1@gmail.com>
2026-03-20 01:32:40 +0000

    função redimensionar separada

--- VetorString.cpp
+++ VetorString.cpp
@@ -9,22 +9,27 @@ void vetorDestruir(VetorString &vetor) {
     vetor.array = 0;
 }
 
+void redimensionarVetor(VetorString &vetor, const float &tam) {
+    int nTam = vetor.tam*tam;
+    string * nArray = new string[nTam];
+    for (int i=0;i<vetor.pos;i++)
+        nArray[i] = vetor.array[i];
+    delete[] vetor.array;
+    vetor.array = nArray;
+    vetor.tam = nTam;
+}
+
 void vetorInserirValor(VetorString &vetor, const string &valor) {
-    if (vetor.pos==vetor.tam) {
-        int nTam = vetor.tam*2;
-        string * nArray = new string[nTam];
-        for (int i=0;i<vetor.pos;i++)
-            nArray[i] = vetor.array[i];
-        delete[] vetor.array;
-        vetor.array = nArray;
-        vetor.tam = nTam;
-    }
+    if (vetor.pos==vetor.tam)
+        redimensionarVetor(vetor, 2);
     vetor.array[vetor.pos++] = valor;
 }
 
 void vetorRemoverValor(VetorString &vetor, const int &indice) {
     if (indice < 0 || indice >= vetor.pos)
         return;
+    if (vetor.pos<=vetor.tam*0.25 && vetor.tam > vetor.tamMin)
+        redimensionarVetor(vetor, 0.5);
     for (int i=indice;i<vetor.pos-1;i++)
         vetor.array[i] = vetor.array[i+1];
     vetor.array[--vetor.pos]="";
