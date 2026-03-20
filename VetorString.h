d42eaa72eaca413ca135495305d923a89267fdec
home <abiliomatute1@gmail.com>
2026-03-20 16:24:32 +0000

    documentação header

--- VetorString.h
+++ VetorString.h
@@ -6,18 +6,38 @@
 #define PROJETO_1_VETOR_H
 #include <string>
 
-using namespace std;
-
 struct VetorString {
-    string * array = new string[4];
+    std::string * array = new std::string[4];
     int tam = 4;
     int pos = 0;
-    const int tamMin = 4;
 };
 
+/**
+ * Liberta a memoria heap ocupada pelo vetor
+ * @param vetor Vetor a ser destruido
+ */
 void vetorDestruir(VetorString &vetor);
-void vetorInserirValor(VetorString &vetor, const string &valor);
+
+/**
+ * Insere string no vetor
+ * @param vetor Vetor a ser modificado
+ * @param valor String que vai ser inserida
+ */
+void vetorInserirValor(VetorString &vetor, const std::string &valor);
+
+/**
+ * Remove valor no indice do vetor e re-organiza o vetor
+ * @param vetor Vetor a ser modificado
+ * @param indice Indice do elemento a ser removido
+ */
 void vetorRemoverValor(VetorString &vetor, const int &indice);
-string vetorBuscarValor(const VetorString &vetor, const int &indice);
+
+/**
+ * Retorna o valor do indice no vetor
+ * @param vetor Vetor alvo
+ * @param indice Indice do vetor
+ * @return String contida no indice
+ */
+std::string vetorBuscarValor(const VetorString &vetor, const int &indice);
 
 #endif //PROJETO_1_VETOR_H
