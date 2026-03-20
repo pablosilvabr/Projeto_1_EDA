5f640da0debb45d94e7cbb7b8e0073709610c37a
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-20 16:26:20 +0000

    função tamanho dos arquivos txt

--- jogador.cpp
+++ jogador.cpp
@@ -16,7 +16,7 @@ using namespace std;
 // };
 
 void pullJogadores(Jogador* lista_jogadores, int& numero_jogadores) {
-    string* lista = leituraNomes();//passar tamanho
+    //string* lista = leituraNomes();//passar tamanho
     int num_por_posicao[4];
 
     num_por_posicao[0] = GR_MIN;
@@ -24,24 +24,19 @@ void pullJogadores(Jogador* lista_jogadores, int& numero_jogadores) {
     num_por_posicao[2] = MED_MIN;
     num_por_posicao[3] = AVA_MIN;
 
-    for (int i=0; i<lista->length(); i++) {
-
-
-
+    for (int i=0; i<tamArquivoNomes(); i++) {
         //incompleto fui dormir, continuo dps :)
 
-
-
         //lista_jogadores[i].nome = lista[i]; //conflito de char* e char -DEBUG
+
         //Informações Jogador
 
-        //lista_jogadores[i].nome =
         lista_jogadores[i].idade = 18 + (rand() % 17);
-        lista_jogadores[i].numero = (rand()%100);//alterado pablo
 
         //Atributos do jogador
 
         int pos = rand() % 4;
+        //lista_jogadores[i].posicao = (rand()%4);
         lista_jogadores[i].qualidade = (rand() % MAX_QUALIDADE) + 1;
         lista_jogadores[i].probLes = (rand() % MAX_PROBLES) + 1; //pablo
         lista_jogadores[i].probSus = (rand() % MAX_PROBSUS) + 1; //pablo
