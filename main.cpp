f7d27904d6d0ecc0011526bc6b26cc71fb16a05b
home <abiliomatute1@gmail.com>
2026-03-20 01:36:26 +0000

    testes vetores string

--- main.cpp
+++ main.cpp
@@ -11,5 +11,12 @@ int main () {
     VetorString lEq = leituraEquipas();
     for (int i=0;i<lEq.pos;i++)
         cout<<vetorBuscarValor(lEq, i)<<endl;
+    cout<<"contagem na lista: "<<lEq.pos<<endl;
+    cout << "lista cheia: "<<lEq.tam<<endl;
+    while (!vetorBuscarValor(lEq, 0).empty())
+        vetorRemoverValor(lEq, 0);
+
+    cout << "lista vazia: "<<lEq.tam<<endl;
+    vetorDestruir(lEq);
     return 0;
 }
