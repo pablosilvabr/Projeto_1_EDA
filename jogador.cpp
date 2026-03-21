ead7e0af5c287349347bc66838dc7dfc45bc5315
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-21 21:29:42 +0000

    Update jogador.cpp

--- jogador.cpp
+++ jogador.cpp
@@ -1,5 +1,3 @@
-//
-// Created by jesus on 03/19/2026.
 #include <iostream>
 #include "utility.h"
 #include "jogador.h"
@@ -44,40 +42,3 @@ Jogador* transferencia() {
     gerarJogadores(novoTransferencia, tam);
     return novoTransferencia;
 }
-
-/*
-Vetor<Jogador> pullJogadores(Vetor<Jogador> lista_jogadores, int& numero_jogadores) {
-    //Vetor<string> lista = leituraNomes();//passar tamanho
-    int num_por_posicao[4];
-
-    num_por_posicao[0] = GR_MIN;
-    num_por_posicao[1] = DEF_MIN;
-    num_por_posicao[2] = MED_MIN;
-    num_por_posicao[3] = AVA_MIN;
-
-    for (int i=0; i<tamArqNomes(); i++) {
-        //incompleto fui dormir, continuo dps :)
-
-        //lista_jogadores[i].nome = nomeAleatorio(); //conflito de char* e char -DEBUG
-
-        //Informações Jogador
-
-        lista_jogadores[i].idade = 18 + (rand() % 17);
-
-        //Atributos do jogador
-
-        int pos = rand() % 4;
-        //lista_jogadores[i].posicao = (rand()%4);
-        lista_jogadores[i].qualidade = (rand() % MAX_QUALIDADE) + 1;
-        lista_jogadores[i].probLes = (rand() % MAX_PROBLES) + 1; //pablo
-        lista_jogadores[i].probSus = (rand() % MAX_PROBSUS) + 1; //pablo
-
-        //lista_jogadores[i].posicao = (num_por_posicao[pos]>enum[pos]);
-
-    }
-    return lista_jogadores;
-}
-*/
-
-
-
