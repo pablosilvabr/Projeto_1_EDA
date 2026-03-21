d12fe2a9c4ae1ae1aea809e4bc844117f986090f
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-21 21:54:13 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,46 +9,8 @@
 using namespace std;
 
 int main() {
+    srand(time(NULL));
     mostrarMenu();
 
     return 0;
 }
-
-
-
-
-
-
-/*
-int main () {
-    srand (time(NULL));
-
-
-
-    return 0;
-}
-*/
-/*
-int main() {
-    srand(time(NULL));
-
-    int tamanhoReal = 0;
-    cout << "A pedir plantel..." << endl;
-
-    Jogador* meuPlantel = plantel(tamanhoReal);
-
-    cout << "Tamanho recebido no main: " << tamanhoReal << endl;
-    cout << "------------------------------------------" << endl;
-
-    if (tamanhoReal == 0) {
-        cout << "ERRO: O tamanho continua a zero. Verifique o '&' na funcao Plantel!" << endl;
-    }
-
-    for (int i = 0; i < tamanhoReal; i++) {
-        cout << i + 1 << ". " << meuPlantel[i].nome << " [" << meuPlantel[i].posicao << "]" << endl;
-    }
-
-    delete[] meuPlantel;
-    return 0;
-}
-*/
