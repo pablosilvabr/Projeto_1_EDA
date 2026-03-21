9eb64e261de6affa274b9daa167f265d463f69bb
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-21 21:29:46 +0000

    Create menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,18 @@
+#include <iomanip>
+#include <iostream>
+
+
+using namespace std;
+
+void mostrarMenu() {
+    cout <<"Nome" << setw(8) << "|"
+    <<"N" << setw(1) << "|"
+    <<"Posicao" << setw(5) << "|"
+    <<"Idade" << setw(5) <<"|"
+    <<"ProbLesao" << setw(5) << "|"
+    <<"ProbCastigo" << setw(5)<<"|"
+    <<"Qualidade" << setw(5) << "|"
+    <<"Dias-Treino" << endl;
+    cout<<"____________________________________________________________________________________________" <<endl;
+
+}
