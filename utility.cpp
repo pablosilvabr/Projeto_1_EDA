eecc752ab47d446e2b6db5baab376d4054f88b15
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-21 20:34:18 +0000

    Update utility.cpp

--- utility.cpp
+++ utility.cpp
@@ -8,69 +8,6 @@
 #include <iostream>
 using namespace std;
 
-
-int tamArqEquipas() {
-    int contador = 0;
-    string linha;
-
-    ifstream arquivoEquipas1(FILE_EQUIPAS);
-
-    if (arquivoEquipas1.is_open()) {
-        while (getline(arquivoEquipas1,linha)) {
-            contador++;
-        }
-        arquivoEquipas1.close();
-    }
-    return contador;
-}
-
-int tamArqNomes() {
-    int contador = 0;
-    string linha;
-
-    ifstream arquivoJogadores1(FILE_NOMES);
-
-    if (arquivoJogadores1.is_open()) {
-        while (getline(arquivoJogadores1,linha)) {
-            contador++;
-        }
-        arquivoJogadores1.close();
-    }
-    return contador;
-}
-
-string* leituraNomes() {
-    int contador = tamArqEquipas();
-
-    string* vNomeJogadores = new string [contador];
-
-    ifstream arquivoJogadores2(FILE_NOMES);
-
-    if (arquivoJogadores2.is_open()) {
-        for (int i = 0; i<contador; i++){
-            getline(arquivoJogadores2,vNomeJogadores[i]);
-        }
-        arquivoJogadores2.close();
-    }
-    return vNomeJogadores;
-}
-
-string* leituraEquipas() {
-    int contador = tamArqEquipas();
-
-    string* vOutrasEquipas = new string [contador];
-
-    ifstream arquivoEquipas2(FILE_EQUIPAS);
-
-    if (arquivoEquipas2.is_open()) {
-        for (int i=0; i<contador; i++){
-            getline(arquivoEquipas2,vOutrasEquipas[i]);
-        }
-        arquivoEquipas2.close();
-    }
-    return vOutrasEquipas;
-}
-
 Vetor<string> ler(const char* ficheiro) {
     Vetor<string> vet;
     string linha;
@@ -91,6 +28,69 @@ Vetor<string> leituraEquipasA() {
 Vetor<string> leituraNomesA() {
     return ler(FILE_NOMES);
 }
+
+int tamArqEquipas() {
+    int contador = 0;
+    string linha;
+
+    ifstream arquivoEquipas("config/equipas");
+
+    if (arquivoEquipas.is_open()) {
+        while (getline(arquivoEquipas,linha)) {
+            contador++;
+        }
+        arquivoEquipas.close();
+    }
+    return contador;
+}
+
+int tamArqNomes() {
+    int contador = 0;
+    string linha;
+
+    ifstream arquivoJogadores("config/nomes");
+
+    if (arquivoJogadores.is_open()) {
+        while (getline(arquivoJogadores,linha)) {
+            contador++;
+        }
+        arquivoJogadores.close();
+    }
+    return contador;
+}
+
+string* leituraNomes() {
+    int contador = tamArqNomes();
+
+    string* vNomeJogadores = new string [contador];
+
+    ifstream arquivoJogadores("config/nomes");
+
+    if (arquivoJogadores.is_open()) {
+        for (int i = 0; i<contador; i++){
+            getline(arquivoJogadores,vNomeJogadores[i]);
+        }
+        arquivoJogadores.close();
+    }
+    return vNomeJogadores;
+}
+
+string* leituraEquipas() {
+    int contador = tamArqEquipas();
+
+    string* vOutrasEquipas = new string [contador];
+
+    ifstream arquivoEquipas2("config/equipas");
+
+    if (arquivoEquipas2.is_open()) {
+        for (int i=0; i<contador; i++){
+            getline(arquivoEquipas2,vOutrasEquipas[i]);
+        }
+        arquivoEquipas2.close();
+    }
+    return vOutrasEquipas;
+}
+
 
 string nomeAleatorio() {
     int indice = tamArqNomes();
