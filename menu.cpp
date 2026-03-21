ffdd47847a39e99a083b81f435057fa4e6745c27
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-21 23:15:43 +0000

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,18 +1,36 @@
 #include <iomanip>
 #include <iostream>
+#include "jogador.h"
 
 
 using namespace std;
 
-void mostrarMenu() {
-    cout <<"Nome" << setw(8) << "|"
-    <<"N" << setw(1) << "|"
-    <<"Posicao" << setw(5) << "|"
-    <<"Idade" << setw(5) <<"|"
-    <<"ProbLesao" << setw(5) << "|"
-    <<"ProbCastigo" << setw(5)<<"|"
-    <<"Qualidade" << setw(5) << "|"
-    <<"Dias-Treino" << endl;
-    cout<<"____________________________________________________________________________________________" <<endl;
 
+
+void mostrarMenu() {
+    int n = 0;
+
+    Jogador* listaJogadores = plantel(n);
+
+
+    cout <<setw(29)<<left<<"Nome" << "| "
+    << setw(8)<<"N"  << "| "
+    <<setw(10) <<"Posicao" <<  "| "
+    <<setw(10) <<"Idade" << "| "
+    <<setw(15) <<"ProbLesao" <<  "| "
+    <<setw(15)<<"ProbCastigo" << "| "
+    <<setw(10) <<"Qualidade" <<  "| "
+    <<setw(10) <<"Dias-Treino" << endl;
+    cout<<"________________________________________________________________________________________________________________________" <<endl;
+    for (int i = 0; i < n; i++) {
+        //colocar o limite do tamanho do plantel
+        cout<<setw(29)<<left<<listaJogadores[i].nome << "| ";
+        cout<<setw(8)<<listaJogadores[i].numero << "| ";
+        cout<<setw(10)<<listaJogadores[i].posicao << "| ";
+        cout<<setw(10)<<listaJogadores[i].idade << "| ";
+        cout<<setw(15)<<listaJogadores[i].probLes << "| ";
+        cout<<setw(15)<<listaJogadores[i].probSus << "| ";
+        cout<<setw(10)<<listaJogadores[i].qualidade << "| ";
+        cout<<setw(10)<<"0"<<endl;
+    }
 }
