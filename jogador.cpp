05fbc23e725555d6696ef898a7c5a7f15732a20a
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-22 13:54:35 +0000

    Update jogador.cpp

--- jogador.cpp
+++ jogador.cpp
@@ -6,78 +6,136 @@
 #include "stdlib.h"
 using namespace std;
 
-Jogador gerarUmJogador() {
+/**
+ * Gerar um jogador com atributos aleatórios
+ * Imprimir os atributos do jogador
+ * Gerar um plantel de jogadores, dependendo da necessidade
+ * Precisa criar regras de Jogadores Mínimos por posição
+ * Precisa ordenar por posição do jogador
+ * Precisa limpar depois no main
+ */
+string nomeAleatorio(string* nomeJogadores, int tamanho) {
+    string nomeJogador = nomeJogadores[numAleatorio(0, tamanho - 1)];
+    return nomeJogador;
+}
+/**
+ * Gerar um jogador com atributos aleatórios
+ * Imprimir os atributos do jogador
+ * Gerar um plantel de jogadores, dependendo da necessidade
+ * Precisa criar regras de Jogadores Mínimos por posição
+ * Precisa ordenar por posição do jogador
+ * Precisa limpar depois no main
+ */
+Jogador gerarUmJogador(string* nomeJogadores, int tamanho) {
     Jogador novo;
 
-    novo.nome = nomeAleatorio();
-    novo.idade = 18 + (rand() % 17);
-    novo.qualidade = 50 + (rand() % 46);//50 à 95 é possível melhorar em 5 qualidade dos jogadores
-    novo.numero = 1 + (rand() % 99);//após estar criado o plantel, devemos verificar numeros repetidos
-    novo.probSus = 5 + (rand()%6);
-    novo.probLes = 5 + (rand()%6);
-    novo.posicao = (rand() % 4);
+    novo.nome = nomeAleatorio(nomeJogadores,tamanho);
+    novo.idade = numAleatorio(18, 40);
+    novo.qualidade = numAleatorio(0, 100);
+    novo.numero = numAleatorio(1, 99);//após estar criado o plantel, devemos verificar numeros repetidos
+    novo.probSus = numAleatorio(0, 20);
+    novo.probLes = numAleatorio(0, 15);
+    novo.posicao = numAleatorio(1, 4);
+    novo.diasTreino = 0;
 
     return novo;
 }
-//gera uma lista de jogadores, dependendo da necessidade
-void gerarJogadores(Jogador* destino, int quantidade) {
-    for (int i = 0; i < quantidade; i++) {
-        destino[i] = gerarUmJogador();
-    }
+/**
+ * Gerar um jogador com atributos aleatórios
+ * Imprimir os atributos do jogador
+ * Gerar um plantel de jogadores, dependendo da necessidade
+ * Precisa criar regras de Jogadores Mínimos por posição
+ * Precisa ordenar por posição do jogador
+ * Precisa limpar depois no main
+ */
+void imprimirJogador(Jogador jogador) {
+    cout << "Nome: " << jogador.nome << endl;
+    cout << "Idade: " << jogador.idade << endl;
+    cout << "Qualidade: " << jogador.qualidade << endl;
+    cout << "Numero: " << jogador.numero << endl;
+    cout << "Probabilidade de Suspensão: " << jogador.probSus << "%" << endl;
+    cout << "Probabilidade de Lesão: " << jogador.probLes << "%" << endl;
+    cout << "Posição: " << jogador.posicao << endl;
+    cout << "Dias de Treino: " << jogador.diasTreino << endl;
 }
+/**
+ * Insere um jogador num array de jogadores, ordenando por posição e número
+ * @param array
+ * @param tamTotal
+ * @param novo
+ * @param capacidadeMax
+ */
+void inserirJogador(Jogador** array, int& tamTotal, Jogador* novo, int capacidadeMax ) {
+    if (tamTotal >= capacidadeMax) {
+        cout << " Nao e posiível inserir mais jogadores." << endl;
+        return;
+    }
+    int i=0;
+    while (i<tamTotal) {
+        if (array[i]->posicao == novo->posicao) break;
+        if (array[i]->posicao == novo->posicao && array[i]->numero> novo->numero) break;
+        i++;
+    }
+    int posInserir = i;
+    for (int j = tamTotal; j > posInserir; j--) {
+        array[j] = array[j - 1];
+    }
+    array[posInserir] = novo;
+    tamTotal++;
 
+}
 //Precisa criar regras de Jogadores Mínimos por posição
 //precisa ordenar
-Jogador* plantel(int& tam) {
+/**Jogador* plantel(int& tam) {
     tam = 20;
     Jogador* novoPlantel = new Jogador[tam];
     gerarJogadores(novoPlantel, tam);
     return novoPlantel;
-}
+}**/
 
 //precisa limpar depois no main
 //precisa ordenar por posição do jogador
-Jogador* transferencia() {
+/**Jogador* transferencia() {
     int tam = 2;
 
     Jogador* novoTransferencia = new Jogador[tam];
     gerarJogadores(novoTransferencia, tam);
     return novoTransferencia;
-}
+}**/
 
-/*
-Vetor<Jogador> pullJogadores(Vetor<Jogador> lista_jogadores, int& numero_jogadores) {
-    //Vetor<string> lista = leituraNomes();//passar tamanho
-    int num_por_posicao[4];
 
-    num_por_posicao[0] = GR_MIN;
-    num_por_posicao[1] = DEF_MIN;
-    num_por_posicao[2] = MED_MIN;
-    num_por_posicao[3] = AVA_MIN;
+// Vetor<Jogador> pullJogadores(Vetor<Jogador> lista_jogadores, int& numero_jogadores) {
+//     //Vetor<string> lista = leituraNomes();//passar tamanho
+//     int num_por_posicao[4];
+//
+//     num_por_posicao[0] = GR_MIN;
+//     num_por_posicao[1] = DEF_MIN;
+//     num_por_posicao[2] = MED_MIN;
+//     num_por_posicao[3] = AVA_MIN;
+//
+//     for (int i=0; i<tamArqNomes(); i++) {
+//         //incompleto fui dormir, continuo dps :)
+//
+//         //lista_jogadores[i].nome = nomeAleatorio(); //conflito de char* e char -DEBUG
+//
+//         //Informações Jogador
+//
+//         lista_jogadores[i].idade = 18 + (rand() % 17);
+//
+//         //Atributos do jogador
+//
+//         int pos = rand() % 4;
+//         //lista_jogadores[i].posicao = (rand()%4);
+//         lista_jogadores[i].qualidade = (rand() % MAX_QUALIDADE) + 1;
+//         lista_jogadores[i].probLes = (rand() % MAX_PROBLES) + 1; //pablo
+//         lista_jogadores[i].probSus = (rand() % MAX_PROBSUS) + 1; //pablo
+//
+//         //lista_jogadores[i].posicao = (num_por_posicao[pos]>enum[pos]);
+//
+//     }
+//     return lista_jogadores;
+// }
 
-    for (int i=0; i<tamArqNomes(); i++) {
-        //incompleto fui dormir, continuo dps :)
-
-        //lista_jogadores[i].nome = nomeAleatorio(); //conflito de char* e char -DEBUG
-
-        //Informações Jogador
-
-        lista_jogadores[i].idade = 18 + (rand() % 17);
-
-        //Atributos do jogador
-
-        int pos = rand() % 4;
-        //lista_jogadores[i].posicao = (rand()%4);
-        lista_jogadores[i].qualidade = (rand() % MAX_QUALIDADE) + 1;
-        lista_jogadores[i].probLes = (rand() % MAX_PROBLES) + 1; //pablo
-        lista_jogadores[i].probSus = (rand() % MAX_PROBSUS) + 1; //pablo
-
-        //lista_jogadores[i].posicao = (num_por_posicao[pos]>enum[pos]);
-
-    }
-    return lista_jogadores;
-}
-*/
 
 
 
