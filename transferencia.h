c7d67189fc194c5dea2237c1fd9cf8fa1e8ded39
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-22 21:27:41 +0000

    Create transferencia.h

---
+++ transferencia.h
@@ -0,0 +1,9 @@
+//
+// Created by pablo on 22/03/2026.
+//
+
+#ifndef PROJETO_1_TRANSFERENCIA_H
+#define PROJETO_1_TRANSFERENCIA_H
+void imprimirTransferencia(Jogador* transferencia, int numJogadoresPlantel);
+Jogador* transferencias (Vetor<std::string> nomeJogadores,int MAXIMO_ADD_TRANSFERENCIA);
+#endif //PROJETO_1_TRANSFERENCIA_H
