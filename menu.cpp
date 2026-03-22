fb5132cce42d231f0777b9c6ea0d05e980bf5ee0
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-22 13:54:23 +0000

    Create menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,38 @@
+//
+// Created by pablo on 22/03/2026.
+//
+#include <iomanip>
+#include <iostream>
+#include "jogador.h"
+
+
+using namespace std;
+/**
+void mostrarMenu() {
+    int n = 0;
+
+    Jogador* listaJogadores = plantel(n);
+
+
+    cout <<setw(29)<<left<<"Nome" << "| "
+    << setw(8)<<"N"  << "| "
+    <<setw(10) <<"Posicao" <<  "| "
+    <<setw(10) <<"Idade" << "| "
+    <<setw(15) <<"ProbLesao" <<  "| "
+    <<setw(15)<<"ProbCastigo" << "| "
+    <<setw(10) <<"Qualidade" <<  "| "
+    <<setw(10) <<"Dias-Treino" << endl;
+    cout<<"________________________________________________________________________________________________________________________" <<endl;
+    for (int i = 0; i < n; i++) {
+        //colocar o limite do tamanho do plantel
+        cout<<setw(29)<<left<<listaJogadores[i].nome << "| ";
+        cout<<setw(8)<<listaJogadores[i].numero << "| ";
+        cout<<setw(10)<<listaJogadores[i].posicao << "| ";
+        cout<<setw(10)<<listaJogadores[i].idade << "| ";
+        cout<<setw(15)<<listaJogadores[i].probLes << "| ";
+        cout<<setw(15)<<listaJogadores[i].probSus << "| ";
+        cout<<setw(10)<<listaJogadores[i].qualidade << "| ";
+        cout<<setw(10)<<"0"<<endl;
+    }
+}
+**/
