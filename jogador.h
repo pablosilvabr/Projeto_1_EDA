f688041fa106cf3ece50fbd885dd9816449c34cc
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-22 13:54:28 +0000

    Update jogador.h

--- jogador.h
+++ jogador.h
@@ -13,7 +13,16 @@ struct Jogador {
     int qualidade;
     int probLes;
     int probSus;
+    int diasTreino;
 };
+std::string nomeAleatorio(std::string* nomeJogadores, int tamanho);
+Jogador gerarUmJogador(std::string* nomeJogadores, int tamanho);
+void imprimirJogador(Jogador jogador);
 
-Jogador* plantel(int& tam);
+void inserirJogador(Jogador** array, int& tamTotal, Jogador* novo, int capacidadeMax );
+//Jogador* plantel(int& tam);
+//Jogador gerarUmJogador();
+//void gerarJogadores(Jogador* destino, int quantidade);
+//Jogador* transferencia();
+
 #endif //PROJETO_1_JOGADOR_H
