656cb4d6bed097e7548880c07aa97c7836ccb5e7
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-03-24 13:27:08 +0000

    Update transferencia.cpp
    Criada função imprimir mercado, e alterada a função mercadoTransferencias, adicionado o limite para o tamanho da lista de nomes

--- transferencia.cpp
+++ transferencia.cpp
@@ -15,12 +15,23 @@ using namespace std;
 Jogador* mercadoTransferencias (Vetor<Jogador> &mercadoTransferencia, Vetor<string> nomeJogadores) {
 
     for (int i = 0; i < MAXIMO_ADD_TRANSFERENCIA; i++) {
-        Jogador novo = gerarJogador(vetorBuscar(nomeJogadores,numAleatorio(0,114)));//precisa alterar para o tamanho do arquivo
+        Jogador novo = gerarJogador(vetorBuscar(nomeJogadores,numAleatorio(0,nomeJogadores.pos-1)));
 
         vetorInserir(mercadoTransferencia, novo);
     }
     return mercadoTransferencia.array;
 }
 
+void imprimirMercado(Vetor<Jogador> &mercado) {
 
-
+    if (mercado.pos == 0) {
+        cout << "O mercado está vazio no momento." << endl;
+    }
+    else {
+        for (int i = 0; i < mercado.pos; i++) {
+            cout << " Nome: " << mercado.array[i].nome << " Posicao: " << mercado.array[i].posicao << " Idade: " << mercado.array[i].idade
+            << " Qualidade: " << mercado.array[i].qualidade << " Prob Lesao: " << mercado.array[i].probLes << "% "
+            << " Prob Suspensao: " << mercado.array[i].probSus << "% " << endl;
+        }
+    }
+}
